08fb8e741b3f080c11f831426b15224399d54f33
Philip Lundrigan <philiplundrigan@gmail.com>
2023-09-26 13:19:50 -0600

    Organize file

--- src/main.cpp
+++ src/main.cpp
@@ -6,26 +6,17 @@ void light_all_lights(int r, int g, int b);
 void turn_off_all_lights();
 bool check_switches();
 
-void setup() {
-    Serial.begin(9600);
-    leds_init();
-    timer_init();
-    delay(1000);
-    main();
-    while (1)
-        ;
-}
-
-void loop() {}
-
+//////////////////////////////
+// Modify this function     //
+//////////////////////////////
 int main() {
     while (true) {
         if (buttons_get(1)) {
             while (buttons_get(1)) {
-                light_all_lights(50, 50, 255);
+                light_all_lights(255, 0, 0);
                 leds_set_brightness(get_brightness());
                 if (check_switches()) {
-                    speaker_play_note(NOTE_C5, 10);
+                    speaker_play_note(NOTE_C4, 10);
                 }
             }
         } else if (buttons_get(2)) {
@@ -51,6 +42,10 @@ int main() {
     }
 }
 
+//////////////////////////////
+// Helper functions         //
+//////////////////////////////
+
 // This function converts the knob's output (1-100) to a brightness value (0-255)
 int get_brightness() {
     int brightness = 255 * knob_get() / 100;
@@ -69,3 +64,20 @@ void turn_off_all_lights() { light_all_lights(0, 0, 0); }
 
 // This function checks if either of the switches are on
 bool check_switches() { return switches_get(1) || switches_get(2); }
+
+//////////////////////////////////////////////////
+// Don't touch these                            //
+// ...unless you know what you are doing        //
+//////////////////////////////////////////////////
+
+void setup() {
+    Serial.begin(9600);
+    leds_init();
+    timer_init();
+    delay(1000);
+    main();
+    while (1)
+        ;
+}
+
+void loop() {}
